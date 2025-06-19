354591df561fb3bbe990ab6449a440150846c56e
Christensen Cadeau <christensencadeau@icloud.com>
2025-06-19 19:22:26 -0400

    Enhance GIF search with dynamic input handling
    - Add form submission handling for dynamic search queries
    - Integrate user-provided search terms into GIPHY API call
    - Update HTML to include searchable form ID
    - Refactor comments and organize a script for readability

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// modal
+// Modal
 const dialog = document.getElementById("gif-select")
 dialog.addEventListener("click", (event) => event.target === dialog && dialog.close())
 
@@ -6,20 +6,35 @@ function showModal() {
     dialog.showModal()
 }
 
-// GIFs
-axios.get(`https://api.giphy.com/v1/gifs/search?api_key=u0KyWi18hDrrxUz86aspP5Qn9aF8bVfb&q=dog&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`)
-    .then((response) => {
-        displayImages(response)
-    })
-    .catch(error => {
-        console.log(error)
-    })
+// Search function
+const form = document.getElementById("gif-search")
+// const searchInput = document.getElementById("search")
 
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    const formData = new FormData(event.target)
+    const formValues = Object.fromEntries(formData.entries())
+    const searchParam = formValues.search
+    // const searchVal = formData.get("search")
+
+    // API call
+    axios.get(`https://api.giphy.com/v1/gifs/search?api_key=u0KyWi18hDrrxUz86aspP5Qn9aF8bVfb&q=${searchParam}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`)
+        .then((response) => {
+            displayImages(response)
+        })
+        .catch(error => {
+            console.log(error)
+        })
+})
+
+
+// GIFs
 const displayImages = (response) => {
     const data = response.data.data
     const gifContainer = document.querySelector(".gif-container")
     gifContainer.innerHTML = ""
-    console.log(data)
+    // console.log(data)
 
     data.forEach((info) => {
         const url = info.images.original.url
