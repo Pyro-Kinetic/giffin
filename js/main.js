ca8f8d40299583a391e58ab2138a55536c3b6923
Christensen Cadeau <christensencadeau@icloud.com>
2025-06-19 20:03:33 -0400

    Refactor GIF search logic and improve image styling
    - Abstract API call logic into a reusable `apiCall` function
    - Trim user input for cleaner search queries
    - Set GIF image height to auto in CSS and SCSS for better responsiveness

--- js/main.js
+++ js/main.js
@@ -15,26 +15,31 @@ form.addEventListener("submit", (event) => {
 
     const formData = new FormData(event.target)
     const formValues = Object.fromEntries(formData.entries())
-    const searchParam = formValues.search
+    const searchParam = formValues.search.trim()
     // const searchVal = formData.get("search")
 
     // API call
-    axios.get(`https://api.giphy.com/v1/gifs/search?api_key=u0KyWi18hDrrxUz86aspP5Qn9aF8bVfb&q=${searchParam}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`)
+    apiCall(searchParam)
+})
+
+
+// GIFs
+const apiCall = (inputString) => {
+    axios.get(`https://api.giphy.com/v1/gifs/search?api_key=u0KyWi18hDrrxUz86aspP5Qn9aF8bVfb&q=${inputString}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`)
         .then((response) => {
             displayImages(response)
         })
         .catch(error => {
             console.log(error)
         })
-})
+}
+apiCall("happy")
 
-
-// GIFs
 const displayImages = (response) => {
     const data = response.data.data
     const gifContainer = document.querySelector(".gif-container")
     gifContainer.innerHTML = ""
-    // console.log(data)
+    console.log(data)
 
     data.forEach((info) => {
         const url = info.images.original.url
