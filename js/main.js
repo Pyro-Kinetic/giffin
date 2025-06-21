80e009d51757de490b313de89e735dde09fc2fd2
Christensen Cadeau <christensencadeau@icloud.com>
2025-06-21 12:15:37 -0400

    Refactor `State` class to improve modularity and scope handling
    - Move `displayImages` into the `State` class for improved encapsulation
    - Update API call logic to use the class-scoped `displayImages` method

--- js/main.js
+++ js/main.js
@@ -42,11 +42,34 @@ class Page {
     apiCall(inputString) {
         axios.get(`https://api.giphy.com/v1/gifs/search?api_key=u0KyWi18hDrrxUz86aspP5Qn9aF8bVfb&q=${inputString}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`)
             .then((response) => {
-                displayImages(response)
+                this.displayImages(response)
             })
             .catch(error => {
                 console.log(error)
             })
+    }
+
+    displayImages(response) {
+        const data = response.data.data
+        const gifContainer = document.querySelector(".gif-container")
+        gifContainer.innerHTML = ""
+
+        console.log("                     ")
+        console.log("displayImages", data)
+        console.log("               ")
+
+        data.forEach((info) => {
+            const url = info.images.original.url
+            const name = info.title
+            const id = info.id
+
+            const imageElement = document.createElement("img")
+            imageElement.src = url
+            imageElement.alt = name
+            imageElement.id = id
+
+            gifContainer.appendChild(imageElement)
+        })
     }
 
     run() {
@@ -54,29 +77,6 @@ class Page {
         this.search()
         console.log(this.state)
     }
-}
-
-
-const displayImages = (response) => {
-    const data = response.data.data
-    const gifContainer = document.querySelector(".gif-container")
-    gifContainer.innerHTML = ""
-    console.log("                     ")
-    console.log("displayImages", data)
-    console.log("               ")
-
-    data.forEach((info) => {
-        const url = info.images.original.url
-        const name = info.title
-        const id = info.id
-
-        const imageElement = document.createElement("img")
-        imageElement.src = url
-        imageElement.alt = name
-        imageElement.id = id
-
-        gifContainer.appendChild(imageElement)
-    })
 }
 
 const main = new State()
