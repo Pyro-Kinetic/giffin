4ae74f36ace12db82c984a08866b90691b857945
Christensen Cadeau <christensencadeau@icloud.com>
2025-06-21 11:54:21 -0400

    Refactor GIF search logic into class methods
    - Move search and API call logic into a reusable class
    - Add reusable `search()` method with form handling
    - Improve modularity by integrating API call into the class structure

--- js/main.js
+++ js/main.js
@@ -23,40 +23,38 @@ class Page {
         this.state = state
     }
 
+    search() {
+        const searchForm = document.getElementById("gif-search")
+
+        searchForm.addEventListener("submit", (event) => {
+            event.preventDefault()
+
+            const formData = new FormData(event.target)
+            const formValues = Object.fromEntries(formData.entries())
+            const searchParam = formValues.search.trim()
+
+            // API call
+            this.apiCall(searchParam)
+        })
+    }
+
+    apiCall(inputString) {
+        axios.get(`https://api.giphy.com/v1/gifs/search?api_key=u0KyWi18hDrrxUz86aspP5Qn9aF8bVfb&q=${inputString}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`)
+            .then((response) => {
+                displayImages(response)
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
 
     run() {
+        this.apiCall("happy")
+        this.search()
         console.log(this.state)
     }
 }
 
-// Search function
-const form = document.getElementById("gif-search")
-// const searchInput = document.getElementById("search")
-
-form.addEventListener("submit", (event) => {
-    event.preventDefault()
-
-    const formData = new FormData(event.target)
-    const formValues = Object.fromEntries(formData.entries())
-    const searchParam = formValues.search.trim()
-    // const searchVal = formData.get("search")
-
-    // API call
-    apiCall(searchParam)
-})
-
-
-// GIFs
-const apiCall = (inputString) => {
-    axios.get(`https://api.giphy.com/v1/gifs/search?api_key=u0KyWi18hDrrxUz86aspP5Qn9aF8bVfb&q=${inputString}&limit=25&offset=0&rating=g&lang=en&bundle=messaging_non_clips`)
-        .then((response) => {
-            displayImages(response)
-        })
-        .catch(error => {
-            console.log(error)
-        })
-}
-apiCall("happy")
 
 const displayImages = (response) => {
     const data = response.data.data
